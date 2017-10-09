17f8177baa474ace3c9cc48abd1461dea7709d8a
jenniferliddle <jennifer@jsquared.co.uk>
2017-10-09 10:35:15 +0100

    Fix consecutive index bug

--- src/i2b.c
+++ src/i2b.c
@@ -480,8 +480,8 @@ static opts_t* i2b_parse_args(int argc, char *argv[])
     if (!opts->library_name) opts->library_name = strdup("unknown");
     if (!opts->sample_alias) opts->sample_alias = strdup(opts->library_name);
     if (!opts->sequencing_centre) opts->sequencing_centre = strdup("SC");
-    if (va_isEmpty(opts->barcode_tag)) va_push(opts->barcode_tag,strdup(DEFAULT_BARCODE_TAG));
-    if (va_isEmpty(opts->quality_tag)) va_push(opts->quality_tag,strdup(DEFAULT_QUALITY_TAG));
+    if (va_isEmpty(opts->barcode_tag)) while (opts->barcode_tag->end < 10) va_push(opts->barcode_tag,strdup(DEFAULT_BARCODE_TAG));
+    if (va_isEmpty(opts->quality_tag)) while (opts->quality_tag->end < 10) va_push(opts->quality_tag,strdup(DEFAULT_QUALITY_TAG));
     if (!opts->platform) opts->platform = strdup("ILLUMINA");
 
     if (!opts->run_folder) {
@@ -894,40 +894,6 @@ static void getCycleRangeFromFile(va_t *cycleRange, xmlDocPtr doc)
 }
 
 /*
- * Check if we have two cycle indexes, which are consecutive, and have *no* secondary barcode tag.
- * If so, merge the two indexes into one.
- */
-static void mergeIndexes(va_t *cycleRange, opts_t *opts)
-{
-    int i1=-1, i2=-1;
-    if (opts->barcode_tag->end > 1) return;     // there is a secondary barcode tag specified
-
-    for (int n = 0; n < cycleRange->end; n++) {
-        cycleRangeEntry_t *cr = cycleRange->entries[n];
-        if (strcmp(cr->readname, "readIndex") == 0) i1 = n;
-        if (strcmp(cr->readname, "readIndex2") == 0) i2 = n;
-    }
-
-    if (i1 > -1 && i2 > -1) {
-        // we have two indexes
-        cycleRangeEntry_t *cr1 = cycleRange->entries[i1];
-        cycleRangeEntry_t *cr2 = cycleRange->entries[i2];
-        if (cr1->last+1 == cr2->first) {
-            // and they look consecutive to me
-            // so merge them
-            cr1->last = cr2->last;
-            // remove i2 entry
-            cycleRange->end--;
-            free(cycleRange->entries[i2]);
-            for (int n=i2; n < cycleRange->end; n++) {
-                cycleRange->entries[n] = cycleRange->entries[n+1];
-            }
-        }
-    }
-}
-
-
-/*
  * Try to find a cycle range from somewhere
  */
 static va_t *getCycleRange(opts_t *opts)
@@ -988,7 +954,6 @@ static va_t *getCycleRange(opts_t *opts)
     }
 
     if (ptr) xmlXPathFreeObject(ptr);
-    mergeIndexes(cycleRange, opts);
     return cycleRange;
 }
 
