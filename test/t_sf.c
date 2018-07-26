c73d0c639611f5fb1db3903ff0ab2b54d6480e7a
jenniferliddle <jennifer@jsquared.co.uk>
2018-07-26 17:01:08 +0100

    Enable multi lane filters

--- test/t_sf.c
+++ test/t_sf.c
@@ -114,27 +114,13 @@ int main(int argc, char**argv)
     snprintf(outputfile, sizeof(outputfile), "%s/sf_filtered.bam", TMPDIR);
     snprintf(cmd, sizeof(cmd), "%s -c -F %s %s", prog, filterfile, MKNAME(DATA_DIR,"/sf.bam"));
     if (system(cmd)) { fprintf(stderr,"Command failed: %s\n",cmd); failure++; }
-    checkFilterFiles(prog, TMPDIR, filterfile, MKNAME(DATA_DIR,"/out/sf_1.filter"));
+    else checkFilterFiles(prog, TMPDIR, filterfile, MKNAME(DATA_DIR,"/out/sf_1.filter"));
 
     // apply filter
     if (verbose) fprintf(stderr,"Applying filter\n");
     snprintf(cmd, sizeof(cmd), "%s -a --verbose -F %s -o %s %s", prog, filterfile, outputfile, MKNAME(DATA_DIR,"/sf.bam"));
     if (system(cmd)) { fprintf(stderr,"Command failed: %s\n",cmd); failure++; }
-    checkFiles(TMPDIR, outputfile, MKNAME(DATA_DIR,"/out/sf_filtered.bam"), verbose);
-
-    // multiple filters
-    if (verbose) fprintf(stderr,"Multiple filters\n");
-    snprintf(outputfile, sizeof(outputfile), "%s/sf_filtered.bam", TMPDIR);
-    snprintf(cmd, sizeof(cmd), "%s -a -v -f -F %s,%s,%s --rg 25077_3#3,25077_4#3,25077_5#3 -o %s %s", prog, MKNAME(DATA_DIR,"/sf_1.filter"), MKNAME(DATA_DIR,"/sf_2.filter"), MKNAME(DATA_DIR,"/sf_3.filter"), outputfile, MKNAME(DATA_DIR,"/sf2.bam"));
-    if (system(cmd)) { fprintf(stderr,"Command failed: %s\n",cmd); failure++; }
-    checkFiles(TMPDIR, outputfile, MKNAME(DATA_DIR,"/out/sf2.bam"), verbose);
-
-    // missing filters
-    if (verbose) fprintf(stderr,"Missing filters\n");
-    snprintf(outputfile, sizeof(outputfile), "%s/sf_filtered_2.bam", TMPDIR);
-    snprintf(cmd, sizeof(cmd), "%s -a -v -f -F %s,%s,%s --rg 25077_3#3,25077_4#3,25077_5#x -o %s %s", prog, MKNAME(DATA_DIR,"/sf_1.filter"), MKNAME(DATA_DIR,"/sf_2.filter"), MKNAME(DATA_DIR,"/sf_3.filter"), outputfile, MKNAME(DATA_DIR,"/sf2.bam"));
-    if (system(cmd)) { fprintf(stderr,"Command failed: %s\n",cmd); failure++; }
-    checkFiles(TMPDIR, outputfile, MKNAME(DATA_DIR,"/out/sf_filtered_2.bam"), verbose);
+    else checkFiles(TMPDIR, outputfile, MKNAME(DATA_DIR,"/out/sf_filtered.bam"), verbose);
 
     printf("spatial_filter tests: %s\n", failure ? "FAILED" : "Passed");
     return failure ? EXIT_FAILURE : EXIT_SUCCESS;
